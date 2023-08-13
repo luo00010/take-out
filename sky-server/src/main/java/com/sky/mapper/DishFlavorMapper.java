d97d18ba22b22c1176861c3f4f4b259226b9a0fa
luo00010 <1851298930@qq.com>
2023-08-13 14:54:23 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,9 +2,12 @@ package com.sky.mapper;
 
 import com.github.pagehelper.Page;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -17,10 +20,11 @@ public interface DishFlavorMapper {
      */
     void inserBath(List<DishFlavor> flavors);
 
+
     /**
-     * 菜品分页查询
-     * @param dishPageQueryDTO
-     * @return
+     * 根据菜品id删除对应的口味数据
+     * @param dishId
      */
-    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
 }
